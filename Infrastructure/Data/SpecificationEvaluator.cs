b09c03dd49ae334903f12318f47262fda12bb495
Bleron Qorri <bleron.qorri@beriflapp.com>
2020-07-13 12:38:46 +0200

    End of section 6

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -16,6 +16,21 @@ namespace Infrastructure.Data
                 query = query.Where(spec.Criteria);
             }
 
+            if(spec.OrderBy != null) 
+            {
+                query = query.OrderBy(spec.OrderBy);
+            }
+
+            if(spec.OrderByDescending != null) 
+            {
+                query = query.OrderByDescending(spec.OrderByDescending);
+            }
+
+            if(spec.isPagingEnabled) // order is important. Paging must come after ordering and filtering
+            {
+                query = query.Skip(spec.Skip).Take(spec.Take); 
+            }
+
             query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
 
             return query;
