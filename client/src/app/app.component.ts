2a29a0804fae7a7b8b9ad69e83d07fa7a5bea99a
Bleron Qorri <bleron.qorri@beriflapp.com>
2020-07-27 22:55:27 +0200

    End of Section 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { IProduct } from './models/product';
-import {IPagination} from './models/pagination';
-
 
 @Component({
   selector: 'app-root',
@@ -11,21 +7,12 @@ import {IPagination} from './models/pagination';
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
-  products: IProduct[];
 
 
   constructor(
-    private httpClient: HttpClient
-  ) {
-
-  }
-
+  ) { }
   ngOnInit(): void {
-    this.httpClient.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: IPagination) => {
-      this.products = response.data;
-    }, error => {
-      console.log(error);
-    });
+
   }
 
 }
